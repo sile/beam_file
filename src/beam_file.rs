d0069747c06658ef59e356defca7393e4730f5e2
Takeru Ohta <phjgt308@gmail.com>
2016-05-20 03:16:32 +0900

    Add BeamFile struct

---
+++ src/beam_file.rs
@@ -0,0 +1,95 @@
+use std::path::Path;
+use std::fs::File;
+use std::io;
+use std::io::Result;
+use std::io::Read;
+use std::io::Write;
+use std::io::Cursor;
+use byteorder::ReadBytesExt;
+use byteorder::WriteBytesExt;
+use byteorder::BigEndian;
+use chunk;
+use chunk::Chunk;
+
+#[derive(Debug)]
+pub struct BeamFile<C> {
+    pub chunks: Vec<C>,
+}
+
+impl<C: Chunk> BeamFile<C> {
+    pub fn get(&self, chunk_id: chunk::Id) -> Option<&C> {
+        self.chunks.iter().find(|c| c.id() == chunk_id)
+    }
+
+    pub fn from_file<P: AsRef<Path>>(path: P) -> Result<Self> {
+        let f = try!(File::open(path));
+        Self::from_reader(f)
+    }
+    pub fn from_reader<R: Read>(mut reader: R) -> Result<Self> {
+        let expected = Header::new(0);
+        let header = try!(Header::from_reader(&mut reader));
+        if header.magic_number != expected.magic_number {
+            return Err(io::Error::new(io::ErrorKind::InvalidData,
+                                      format!("Unexpected magic number: {:?}",
+                                              header.magic_number)));
+        }
+        if header.type_id != expected.type_id {
+            return Err(io::Error::new(io::ErrorKind::InvalidData,
+                                      format!("Unexpected type: {:?}", header.type_id)));
+        }
+
+        let mut buf = vec![0; (header.payload_size - 4) as usize];
+        try!(reader.read_exact(&mut buf));
+
+        let mut chunks = Vec::new();
+        let mut cursor = Cursor::new(&buf);
+        while cursor.position() < buf.len() as u64 {
+            chunks.push(try!(C::decode(&mut cursor)));
+        }
+        Ok(BeamFile { chunks: chunks })
+    }
+
+    pub fn to_file<P: AsRef<Path>>(&self, path: P) -> Result<()> {
+        let f = try!(File::create(path));
+        self.to_writer(f)
+    }
+    pub fn to_writer<W: Write>(&self, mut writer: W) -> Result<()> {
+        let mut buf = Vec::new();
+        for chunk in &self.chunks {
+            try!(chunk.encode(&mut buf));
+        }
+
+        let header = Header::new(buf.len() as u32);
+        try!(header.to_writer(&mut writer));
+        try!(writer.write_all(&buf));
+        Ok(())
+    }
+}
+
+struct Header {
+    magic_number: [u8; 4],
+    payload_size: u32,
+    type_id: [u8; 4],
+}
+impl Header {
+    fn new(payload_size: u32) -> Self {
+        Header {
+            magic_number: *b"FOR1",
+            payload_size: payload_size,
+            type_id: *b"BEAM",
+        }
+    }
+    fn from_reader<R: Read>(mut reader: R) -> Result<Self> {
+        let mut header = Self::new(0);
+        try!(reader.read_exact(&mut header.magic_number));
+        header.payload_size = try!(reader.read_u32::<BigEndian>());
+        try!(reader.read_exact(&mut header.type_id));
+        Ok(header)
+    }
+    fn to_writer<W: Write>(&self, mut writer: W) -> Result<()> {
+        try!(writer.write_all(&self.magic_number));
+        try!(writer.write_u32::<BigEndian>(self.payload_size));
+        try!(writer.write_all(&self.type_id));
+        Ok(())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
 extern crate byteorder;
 
 pub mod chunk;
+mod beam_file;
+
+pub type BeamFile<C> = beam_file::BeamFile<C>;
+pub type RawBeamFile = beam_file::BeamFile<chunk::RawChunk>;
+pub type StandardBeamFile = beam_file::BeamFile<chunk::StandardChunk>;
