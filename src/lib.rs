8c55169b7c3f63f833138cf9795f6c9b8b82b9d5
Takeru Ohta <phjgt308@gmail.com>
2016-05-21 01:41:31 +0900

    Use re-export instead of type re-definition

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,6 @@ pub mod chunk;
 pub mod parts;
 mod beam_file;
 
-pub type BeamFile<C> = beam_file::BeamFile<C>;
-pub type RawBeamFile = beam_file::BeamFile<chunk::RawChunk>;
-pub type StandardBeamFile = beam_file::BeamFile<chunk::StandardChunk>;
+pub use beam_file::BeamFile;
+pub type RawBeamFile = BeamFile<chunk::RawChunk>;
+pub type StandardBeamFile = BeamFile<chunk::StandardChunk>;
