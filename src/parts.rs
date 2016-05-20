38f735cbde8e6361a7ee101685f9c31ae64c6b98
Takeru Ohta <phjgt308@gmail.com>
2016-05-20 19:33:18 +0900

    mv src/parts/mod.rs src/parts.rs

---
+++ src/parts.rs
@@ -0,0 +1,21 @@
+pub type AtomId = u32;
+pub type Arity = u32;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Atom {
+    pub name: String,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Import {
+    pub module: AtomId,
+    pub function: AtomId,
+    pub arity: Arity,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Export {
+    pub function: AtomId,
+    pub arity: Arity,
+    pub label: u32,
+}
