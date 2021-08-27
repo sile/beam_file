154a6ca54d24c2763cfed8d9b680e314f18ab241
Roman Gafiyatullin <r.gafiyatullin@me.com>
2021-08-27 13:49:24 +0300

    try! macro

--- src/beam_file.rs
+++ src/beam_file.rs
@@ -21,12 +21,12 @@ pub struct BeamFile<C> {
 }
 impl<C: Chunk> BeamFile<C> {
     pub fn from_file<P: AsRef<Path>>(path: P) -> Result<Self> {
-        let f = r#try!(File::open(path));
+        let f = File::open(path)?;
         Self::from_reader(f)
     }
     pub fn from_reader<R: Read>(mut reader: R) -> Result<Self> {
         let expected = Header::new(0);
-        let header = r#try!(Header::from_reader(&mut reader));
+        let header = Header::from_reader(&mut reader)?;
         if header.magic_number != expected.magic_number {
             return Err(Error::UnexpectedMagicNumber {
                 magic_number: header.magic_number,
@@ -39,29 +39,29 @@ impl<C: Chunk> BeamFile<C> {
         }
 
         let mut buf = vec![0; (header.payload_size - 4) as usize];
-        r#try!(reader.read_exact(&mut buf));
+        reader.read_exact(&mut buf)?;
 
         let mut chunks = Vec::new();
         let mut cursor = Cursor::new(&buf);
         while cursor.position() < buf.len() as u64 {
-            chunks.push(r#try!(C::decode(&mut cursor)));
+            chunks.push(C::decode(&mut cursor)?);
         }
         Ok(BeamFile { chunks })
     }
 
     pub fn to_file<P: AsRef<Path>>(&self, path: P) -> Result<()> {
-        let f = r#try!(File::create(path));
+        let f = File::create(path)?;
         self.to_writer(f)
     }
     pub fn to_writer<W: Write>(&self, mut writer: W) -> Result<()> {
         let mut buf = Vec::new();
         for chunk in &self.chunks {
-            r#try!(chunk.encode(&mut buf));
+            chunk.encode(&mut buf)?;
         }
 
         let header = Header::new(buf.len() as u32 + 4);
-        r#try!(header.to_writer(&mut writer));
-        r#try!(writer.write_all(&buf));
+        header.to_writer(&mut writer)?;
+        writer.write_all(&buf)?;
         Ok(())
     }
 }
@@ -81,15 +81,15 @@ impl Header {
     }
     fn from_reader<R: Read>(mut reader: R) -> Result<Self> {
         let mut header = Self::new(0);
-        r#try!(reader.read_exact(&mut header.magic_number));
-        header.payload_size = r#try!(reader.read_u32::<BigEndian>());
-        r#try!(reader.read_exact(&mut header.type_id));
+        reader.read_exact(&mut header.magic_number)?;
+        header.payload_size = reader.read_u32::<BigEndian>()?;
+        reader.read_exact(&mut header.type_id)?;
         Ok(header)
     }
     fn to_writer<W: Write>(&self, mut writer: W) -> Result<()> {
-        r#try!(writer.write_all(&self.magic_number));
-        r#try!(writer.write_u32::<BigEndian>(self.payload_size));
-        r#try!(writer.write_all(&self.type_id));
+        writer.write_all(&self.magic_number)?;
+        writer.write_u32::<BigEndian>(self.payload_size)?;
+        writer.write_all(&self.type_id)?;
         Ok(())
     }
 }
